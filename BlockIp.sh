4ca491cc5630efb44a1503a869a68f98cf7e142c
xfaraday <theonlyfaraday@gmail.com>
2023-02-16 17:45:15 -0800

    up ;)

--- BlockIp.sh
+++ BlockIp.sh
@@ -1,10 +1,51 @@
 #!/bin/bash
 
 # Check if IP address was passed as argument
+if [ "$EUID" -ne 0 ]; then
+        echo 'script requires root privileges'
+        exit 1
+fi
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <IP address>"
     exit 1
 fi
+
+if [ -d "/opt/memento" ]; then
+	continue
+else 
+	mkdir /opt/memento
+fi
+
+#file exists
+#check if head -n 1 = reset
+
+#if file doesnt exist
+#create file and echo "reset" > firewall.log
+#then perform block action
+
+if [ -f "/opt/memento/firewallip.log" ]; then
+	if [ $(head -n 1 /opt/memento/firewallip.log) = "reset" ]; then
+		echo "blocking $1" >> /opt/memento/firewallip.log
+	else
+		echo "reset" >> /opt/memento/firewallip.log
+		echo "blocking $1" >> /opt/memento/firewallip.log
+		iptables -P INPUT ACCEPT
+		iptables -P OUTPUT ACCEPT
+		iptables -P FORWARD ACCEPT
+		iptables -F
+		iptables-save
+	fi
+else
+	echo "reset" >> /opt/memento/firewallip.log
+	echo "blocking $1" >> /opt/memento/firewallip.log
+	iptables -P INPUT ACCEPT
+	iptables -P OUTPUT ACCEPT
+	iptables -P FORWARD ACCEPT
+	iptables -F
+	iptables-save
+fi
+
 
 # Store the IP address
 IP=$1
--- BlockPort.sh
+++ BlockPort.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
 
 # Check if port number was passed as argument
+if [ "$EUID" -ne 0 ]; then
+        echo 'script requires root privileges'
+        exit 1
+fi
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <port number>"
     exit 1
+fi
+
+if [ -d "/opt/memento" ]; then
+	continue
+else 
+	mkdir /opt/memento
+fi
+
+#file exists
+#check if head -n 1 = reset
+
+#if file doesnt exist
+#create file and echo "reset" > firewall.log
+#then perform block action
+
+if [ -f "/opt/memento/firewallport.log" ]; then
+	if [ $(head -n 1 /opt/memento/firewallport.log) = "reset" ]; then
+		echo "blocking $1" >> /opt/memento/firewallport.log
+	else
+		echo "reset" >> /opt/memento/firewallport.log
+		echo "blocking $1" >> /opt/memento/firewallport.log
+		iptables -P INPUT ACCEPT
+		iptables -P OUTPUT ACCEPT
+		iptables -P FORWARD ACCEPT
+		iptables -F
+		iptables-save
+	fi
+else
+	echo "reset" >> /opt/memento/firewallport.log
+	echo "blocking $1" >> /opt/memento/firewallport.log
+	iptables -P INPUT ACCEPT
+	iptables -P OUTPUT ACCEPT
+	iptables -P FORWARD ACCEPT
+	iptables -F
+	iptables-save
 fi
 
 # Store the port number
